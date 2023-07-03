4a47874c97031bd119a67a96d40698ac242c6c8c
EC2 Default User <ec2-user@ip-172-31-33-6.ap-northeast-1.compute.internal>
2023-07-03 07:32:53 +0000

    [Add]レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    root 'homes#top', as: 'root'
+  end
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  namespace :public do
+    root 'homes#top', as: 'root'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+    email: 'test@gmail.com',
+    password: '20000404',
+)
+
+Customer.create!(
+    {
+      first_name: "名前",
+      last_name: "苗字",
+      first_name_kana: "ナマエ",
+      last_name_kana: "ミョウジ",
+      email: "test1@gmail.com",
+      postal_code: "1111111",
+      address: "test1県test1市",
+      telephone_number: "11111111111",
+      password: "111111",
+    }
+    )
